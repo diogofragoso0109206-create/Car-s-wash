9cc6f765899d25fa514dea3ad85e3c3e7381f3ad
Diogo Fragoso <diogo.fragoso.0109206@gmail.com>
2026-01-08 10:40:37 +0000

    botons resolvidos

--- PHP/inserirdados.php
+++ PHP/inserirdados.php
@@ -7,12 +7,16 @@ $passwor = $_POST['passwor'];
 
 include 'ligaDB.php';
 
+
 $query = "INSERT INTO user (nome, email, passwor ) VALUES ('$nome', '$email', '$passwor')";
 
 if (mysqli_query($connect, $query)) {
-    echo "<script>alert('Registo efetuado com sucesso'); window.location='../registaruser.html';</script>";
+    echo "<script>alert('Registo efetuado com sucesso'); window.location='../index.html';</script>";
+  
 } else {
     echo "<script>alert('Erro ao efetuar registo'); window.location='../registaruser.html';</script>";
 }
 
 ?>
+
+
--- PHP/inseriremails.php
+++ PHP/inseriremails.php
@@ -11,7 +11,7 @@ include 'ligaDB.php';
 $query = "INSERT INTO emails (nome, email, phone, messages ) VALUES ('$nome', '$email', '$phone', '$messages')";
 
 if (mysqli_query($connect, $query)) {
-    echo "<script>alert('Registo efetuado com sucesso'); window.location='../registaruser.html';</script>";
+    echo "<script>alert('Registo efetuado com sucesso'); window.location='../index.html';</script>";
 } else {
     echo "<script>alert('Erro ao efetuar registo'); window.location='../registaruser.html';</script>";
 }
